5c7c00d0d61c63e836cff06490b8a4ddcfdd2454
Magus Vagus <magusvagus@proton.me>
2025-05-28 19:46:49 +0000

    added slstatus

---
+++ slstatus/slstatus/custom_scripts/cpu_temp.sh
@@ -0,0 +1,16 @@
+temp="$(sensors | awk '/Core 0/ {print $3 } ' | tr -d \"+°C\")"
+temp=${temp%.*}
+#echo " $temp"
+
+yellow="^c#b27e3e^"
+red="^c#924747^"
+block="^c#a36943^"
+debug="^c#ffffff^"
+
+if [[ $temp -lt 30 ]]; then
+	echo "${yellow} $temp${block}"
+elif [[ $temp -ge 80 ]]; then
+	echo "${red} $temp${block}"
+else
+	echo " $temp"
+fi
